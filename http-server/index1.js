eb3346d7daaf9da976ce89cd916e2b03bcf5eff9
Laxmi Prasanna Bethi <127977308+Laxmibethi@users.noreply.github.com>
2023-08-23 21:02:36 +0530

    Create index1.js

---
+++ http-server/index1.js
@@ -0,0 +1,50 @@
+let user_det=document.getElementById("f");
+
+const retent=()=> {
+    let ent= localStorage.getItem("f1");
+    if(ent){
+        ent=JSON.parse(ent);
+    }
+    else{
+        ent=[];
+    }
+    return ent;
+}
+let userDet=retent();
+const dispent=()=> {
+    const ent=retent();
+    let tableEntries = '';
+    for (const entry of ent) {
+        const nameCell = `<td>${entry.name}</td>`;
+        const emailCell = `<td>${entry.email}</td>`;
+        const passwordCell = `<td>${entry.password}</td>`;
+        const dobCell = `<td>${entry.dob}</td>`;
+        const acceptTermsCell = `<td>${entry.acceptTerms ? 'true' : 'false'}</td>`;
+      
+        const row = `<tr>${nameCell}${emailCell}${passwordCell}${dobCell}${acceptTermsCell}</tr>`;
+        tableEntries += row;
+      }
+    const table = `<table><tr><th>Name</th><th>Email</th><th>Password</th><th>Dob</th><th>Accepted terms?</th></tr>${tableEntries}</table>`;
+    let details= document.getElementById("f1");
+    details.innerHTML=table;
+} 
+const SaveDet=(event)=>{
+    event.preventDefault();
+    const name=document.getElementById("name").value;
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const dob=document.getElementById("dob").value;
+    const acceptTerms=document.getElementById("acceptTerms").value;
+const entry={
+    name,
+    email,
+    password,
+    dob,
+    acceptTerms
+};
+userDet.push(entry);
+localStorage.setItem("f1",JSON.stringify(userDet));
+dispent();
+}
+user_det.addEventListener("submit",SaveDet);
+dispent();
